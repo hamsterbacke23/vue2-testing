315951f902abacb0fcffe46e50b64e3544053dae
hamsterbacke23 <tobias.schmidt82@gmail.com>
2022-09-20 10:18:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue2-vue-cli-gxvyc2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue2-vue-cli-gxvyc2)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <ul>
+    <li><a href="#">Space</a></li>
+    <li><a href="#">Space</a></li>
+    <li><a href="#">Space</a></li>
+    <li>
+      <a href="#settings">Settings</a>
+      <ul>
+        <li><a href="#">Space</a></li>
+        <li><a href="#">Space</a></li>
+        <li><a href="#">Space</a></li>
+      </ul>
+    </li>
+    <li><a href="#">Space</a></li>
+  </ul>
+</template>
+
+<style scoped lang="postcss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap');
+
+body {
+  font-family: 'Roboto';
+  padding: 40px;
+}
+
+li {
+  width: 200px;
+  list-style-type: none;
+  padding: 0;
+  margin: 0 0 20px 0;
+
+  & a {
+    border-top: 2px solid #6e6e6e;
+    padding-top: 5px;
+    display: block;
+    text-decoration: none;
+    font-weight: 500;
+    color: #6e6e6e;
+
+    &:hover,
+    &:focus {
+      color: #3e3e3e;
+      border-color: #3e3e3e;
+    }
+  }
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  position: relative;
+
+  & ul {
+    position: absolute;
+  }
+}
+</style>
